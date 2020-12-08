1c291dc223b00a5c1c763fff9bfac50dc6399650
Motahar Zaman <motaharz95@gmail.com>
2020-12-08 17:53:58 +0600

    controller method for (redmine issue-218) email sent to project owners which left 3 days to end

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\Common;
+use App\Models\Project;
 use Illuminate\Http\Request;
+use Mail;
 
 class HomeController extends Controller
 {
@@ -24,5 +27,49 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function warnMailToProjectOwner(Request $request){
+        $projects = Project::with('user')->with('investment')
+            ->where('status', 1)
+            ->where('end','<=', date("Y-m-d", strtotime('+3 days')))
+            ->where('end','>', date("Y-m-d", strtotime('+2 days')))
+            ->get();
+        foreach ($projects as $project){
+            $totalAmount = $project->investment->sum('amount');
+            $percentage = round(($totalAmount/$project->budget)*100, 2);
+            if($project['is_100%']){
+                $emailData = [
+                    'name' => $project->user->first_name .' '. $project->user->last_name,
+                    'register_token' => $project->user->register_token,
+                    'subject' => '【Crofun】支援プロジェクトの経過報告',
+                    'from_email' => 'noreply@crofun.jp',
+                    'from_name' => 'Crofun',
+                    'template' => 'user.email.projectTargetAmountFullfilled',
+                    'root'     => $request->root(),
+                    'email'     => $project->user->email,
+                    'project_name'  => $project->title,
+                    'amount'  => $totalAmount,
+                    'percentage' => $percentage,
+                ];
+            }
+            else{
+                $emailData = [
+                    'name' => $project->user->first_name .' '. $project->user->last_name,
+                    'register_token' => $project->user->register_token,
+                    'subject' => '【Crofun】支援プロジェクトの経過報告',
+                    'from_email' => 'noreply@crofun.jp',
+                    'from_name' => 'Crofun',
+                    'template' => 'user.email.projectTargetAmountNotFullfill',
+                    'root'     => $request->root(),
+                    'email'     => $project->user->email,
+                    'project_name'  => $project->title,
+                    'amount'  => $totalAmount,
+                    'percentage' => $percentage,
+                ];
+            }
+            Mail::to($project->user->email)->send(new Common($emailData));
+        }
+        return "yes";
     }
 }
