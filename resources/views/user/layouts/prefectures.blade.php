08390170c84e30d5eeacbf4430f832d167aeaa52
Motahar Zaman <motaharz95@gmail.com>
2021-01-05 17:16:23 +0600

    old data filled up to profile complete

--- resources/views/user/layouts/prefectures.blade.php
+++ resources/views/user/layouts/prefectures.blade.php
@@ -1,17 +1,15 @@
 <?php
-$Data = new App\Helpers\Data();
-$prefectures = $Data->prefectures;
-// dd($sorts);
+	$Data = new App\Helpers\Data();
+	$prefectures = $Data->prefectures;
 ?>
 
 <select class="form-control required prefectures" id="prefectures" name="prefectures" onclick="removealert('prefecture_error')">
 	<option value="">選択</option>
 	<?php foreach($prefectures as $p){?>
-		<option value="{{$p['value']}}" {{ isset($user->profile->prefectures) && $user->profile->prefectures == $p['value'] ? 'selected' : '' }}>{{$p['name']}}</option>
+		<option value="{{$p['value']}}" {{ old('prefectures') == $p['value'] ? 'selected' : '' }}>{{$p['name']}}</option>
 	<?php }?>
 </select>
 <span class="error_font" id="prefecture_error" style="color:red;"></span>
-
 
 @section('sort_js')
 
