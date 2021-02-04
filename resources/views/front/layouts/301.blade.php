6281476f57c4de172b7d81efba3c071142199bb8
Motahar Zaman <motaharz95@gmail.com>
2021-02-04 12:17:34 +0600

    code re-formation and cleanup

--- resources/views/front/layouts/301.blade.php
+++ resources/views/front/layouts/301.blade.php
@@ -11,10 +11,8 @@
 										<div class="bg-white p-2">
 											<h4 class="pb-2 font-weight-bold">商品詳細</h4>
 											<p>{!!nl2br(e($product->explanation))!!}</p>
-
 										</div>
 									</div>
-
 									<div class="content-inner mb-3 ">
 										<div class="bg-white p-2">
 											<h4 class="pb-2 font-weight-bold">掲載者情報</h4>
@@ -43,81 +41,78 @@
 													{{-- <button type="button" class="btn btn-md mt-4 text-white w6-14" name="button" style=""> <span class="fa fa-envelope" style="color:#fff;"></span> メッセージを送る</button> --}}
 												</div>
 											</div>
-
-										</div>
-									</div>
-							</div>
-							<div class="col-md-4">
-								@foreach ($any_two as $product)
-								<div class="content-inner mb-3">
-									<div class="bg-white p-2">
-										<div class=row>
-											<div class="px-4 mb-2 col-md-12">
-												<h4 class="fornt-weight-bold p-0">
-													<a href="{{route('front-product-details', ['id' => $product->id])}}" style="color:#6e6e6e">{{ $product->title }}</a>
-												</h4>
-												{{-- <span>リターン名がここに入りますす</span> --}}
-
-											</div>
-											<div class="px-4 mb-2 col-md-12">
-												<a href="{{route('front-product-details', ['id' => $product->id])}}" style="color:#6e6e6e">
-													<img src="{{$product->image ? Request::root().'/uploads/products/'.$product->image : asset('uploads/products/1615154785167836.jpeg')}}" alt="" class="" width="100%" height="200px" style="object-fit:contain">
-												</a>
-											</div>
-											<div class="px-4 mb-2 col-md-12" style="font-size:15px;">
-												<h4>{!!number_format($product->price)!!} ポイント</h4>
-												CrofunポイントとはCrofunに登録されて いる様々な商品と交換できるポイントです。
-											</div>
-											@if(Auth::check())
-												@if($product->user_id == Auth::user()->id)
-													<div class="px-4 mb-2 mt-1 col-md-12">
-														<form action="" method="">
-															{{--<input type="hidden" name="id" value="{{$product->id}}">
-															<input type="hidden" name="price" value="{{$product->price}}">
-															<input type="hidden" name="title" value="{{$product->title}}">
-															<input type="hidden" name="quantity" class="add_to_cart_quantity form-control" value="1" required placeholder="数量">--}}
-															<button type="" class="px-3 text-white btn btn-lg btn-block addCart w6-18" style="background-color:#cccccc !important; height:80%;cursor: pointer;"> <span class="fa fa-shopping-cart px-1"></span> カートに入れる</button>
-															{{-- <div class=" div-radius1 m-0 p-0" style="height:50px"> --}}
-															{{-- </div> --}}
-														</form>
-													</div>
-												@else
-													<div class="px-4 mb-2 mt-1 col-md-12">
-														<form action="{{route('front-cart-add')}}" method="get">
-															<input type="hidden" name="id" value="{{$product->id}}">
-															<input type="hidden" name="price" value="{{$product->price}}">
-															<input type="hidden" name="title" value="{{$product->title}}">
-															<input type="hidden" name="quantity" class="add_to_cart_quantity form-control" value="1" required placeholder="数量">
-															<button type="submit" class="px-3 text-white btn btn-lg btn-block addCart w6-18 btn-warning" style="height:80%;cursor: pointer;"> <span class="fa fa-shopping-cart px-1"></span> カートに入れる</button>
-															{{-- <div class=" div-radius1 m-0 p-0" style="height:50px"> --}}
-															{{-- </div> --}}
-														</form>
-													</div>
-												@endif
-											@else
-												<div class="px-4 mb-2 mt-1 col-md-12">
-													<form action="{{route('front-product-details-login', ['id' => $product->id])}}" method="get">
-														<input type="hidden" name="id" value="{{$product->id}}">
-														<input type="hidden" name="price" value="{{$product->price}}">
-														<input type="hidden" name="title" value="{{$product->title}}">
-														<input type="hidden" name="quantity" class="add_to_cart_quantity form-control" value="1" required placeholder="数量">
-														<button type="submit" class="px-3 text-white btn btn-lg btn-block addCart w6-18 btn-warning" style="height:80%;cursor: pointer;"> <span class="fa fa-shopping-cart px-1"></span> カートに入れる</button>
-														{{-- <div class=" div-radius1 m-0 p-0" style="height:50px"> --}}
-														{{-- </div> --}}
-													</form>
-												</div>
-											@endif
 										</div>
 									</div>
 								</div>
-							@endforeach
-							</div>
+								<div class="col-md-4">
+									@foreach ($any_two as $product)
+										<div class="content-inner mb-3">
+											<div class="bg-white p-2">
+												<div class=row>
+													<div class="px-4 mb-2 col-md-12">
+														<h4 class="fornt-weight-bold p-0">
+															<a href="{{route('front-product-details', ['id' => $product->id])}}" style="color:#6e6e6e">{{ $product->title }}</a>
+														</h4>
+														{{-- <span>リターン名がここに入りますす</span> --}}
 
+													</div>
+													<div class="px-4 mb-2 col-md-12">
+														<a href="{{route('front-product-details', ['id' => $product->id])}}" style="color:#6e6e6e">
+															<img src="{{$product->image ? Request::root().'/uploads/products/'.$product->image : asset('uploads/products/1615154785167836.jpeg')}}" alt="" class="" width="100%" height="200px" style="object-fit:contain">
+														</a>
+													</div>
+													<div class="px-4 mb-2 col-md-12" style="font-size:15px;">
+														<h4>{!!number_format($product->price)!!} ポイント</h4>
+														CrofunポイントとはCrofunに登録されて いる様々な商品と交換できるポイントです。
+													</div>
+													@if(Auth::check())
+														@if($product->user_id == Auth::user()->id)
+															<div class="px-4 mb-2 mt-1 col-md-12">
+																<form action="" method="">
+																	{{--<input type="hidden" name="id" value="{{$product->id}}">
+																	<input type="hidden" name="price" value="{{$product->price}}">
+																	<input type="hidden" name="title" value="{{$product->title}}">
+																	<input type="hidden" name="quantity" class="add_to_cart_quantity form-control" value="1" required placeholder="数量">--}}
+																	<button type="" class="px-3 text-white btn btn-lg btn-block addCart w6-18" style="background-color:#cccccc !important; height:80%;cursor: pointer;"> <span class="fa fa-shopping-cart px-1"></span> カートに入れる</button>
+																	{{-- <div class=" div-radius1 m-0 p-0" style="height:50px"> --}}
+																	{{-- </div> --}}
+																</form>
+															</div>
+														@else
+															<div class="px-4 mb-2 mt-1 col-md-12">
+																<form action="{{route('front-cart-add')}}" method="get">
+																	<input type="hidden" name="id" value="{{$product->id}}">
+																	<input type="hidden" name="price" value="{{$product->price}}">
+																	<input type="hidden" name="title" value="{{$product->title}}">
+																	<input type="hidden" name="quantity" class="add_to_cart_quantity form-control" value="1" required placeholder="数量">
+																	<button type="submit" class="px-3 text-white btn btn-lg btn-block addCart w6-18 btn-warning" style="height:80%;cursor: pointer;"> <span class="fa fa-shopping-cart px-1"></span> カートに入れる</button>
+																	{{-- <div class=" div-radius1 m-0 p-0" style="height:50px"> --}}
+																	{{-- </div> --}}
+																</form>
+															</div>
+														@endif
+													@else
+														<div class="px-4 mb-2 mt-1 col-md-12">
+															<form action="{{route('front-product-details-login', ['id' => $product->id])}}" method="get">
+																<input type="hidden" name="id" value="{{$product->id}}">
+																<input type="hidden" name="price" value="{{$product->price}}">
+																<input type="hidden" name="title" value="{{$product->title}}">
+																<input type="hidden" name="quantity" class="add_to_cart_quantity form-control" value="1" required placeholder="数量">
+																<button type="submit" class="px-3 text-white btn btn-lg btn-block addCart w6-18 btn-warning" style="height:80%;cursor: pointer;"> <span class="fa fa-shopping-cart px-1"></span> カートに入れる</button>
+																{{-- <div class=" div-radius1 m-0 p-0" style="height:50px"> --}}
+																{{-- </div> --}}
+															</form>
+														</div>
+													@endif
+												</div>
+											</div>
+										</div>
+									@endforeach
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-
 			</div>
 		</div>
 	</div>
--- resources/views/user/layouts/message_modal.blade.php
+++ resources/views/user/layouts/message_modal.blade.php
@@ -1,42 +1,42 @@
 {{-- <div class="modal fade" id="{{isset($message_modal_id)?$message_modal_id:'message'}}"> --}}
 <div id="send-message" class="modal fade" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form action="{{route('user-send-message')}}" method="post">
+                {!! csrf_field() !!}
+                <input type="hidden" name="to_id" value="" id="to_id">
+                <div class="modal-header">
+                    <h5 class="modal-title px-3 col-10"><span id="modal-title"></span></h5>
+                    <button type="button" class="close col-2 text-right" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
 
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <form action="{{route('user-send-message')}}" method="post">
-        {!! csrf_field() !!}
-        <input type="hidden" name="to_id" value="" id="to_id">
-        <div class="modal-header">
-          <h5 class="modal-title px-3 col-10"><span id="modal-title"></span></h5>
-            <button type="button" class="close col-2 text-right" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-        
+                </div>
+                <div class="modal-body modal_body_area row">
+                    <p class="col-12 mb-0 pb-0" id="get_vendor_name"></p>
+                    <div class="form-group col-md-12 mt-0 pt-0">
+                        {{-- <label for="">宛先</label> --}}
+                        <input class="form-control" type="hidden" name="name"
+                               value="{{ Auth::user()->first_name.' '.Auth::user()->last_name }}" requried readonly>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="">件名</label>
+                        <input class="form-control" name="subject" placeholder="件名" required maxlength="100">
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="">内容</label>
+                        <textarea name="message" rows="4" cols="80" class="form-control" placeholder="内容" required
+                                  maxlength="2000"></textarea>
+                    </div>
+                    <div class="col-md-12">
+                        <h4 class="text-center text-white">
+                            <button id="submit_message" type="submit" class="btn btn-warning text-white" style="background:#0099ff;">
+                                メッセージ送信
+                            </button>
+                        </h4>
+                    </div>
+                </div>
+            </form>
         </div>
-        <div class="modal-body modal_body_area row">
-          <p class="col-12 mb-0 pb-0" id="get_vendor_name"></p>
-          <div class="form-group col-md-12 mt-0 pt-0">
-			         {{-- <label for="">宛先</label> --}}
-			     <input class="form-control" type="hidden" name="name" value="{{ Auth::user()->first_name.' '.Auth::user()->last_name }}" requried readonly>
-		     </div>
-        <div class="form-group col-md-12">
-			       <label for="">件名</label>
-			          <input class="form-control" name="subject" placeholder="件名" required maxlength="100">
-		     </div>
-        <div class="form-group col-md-12">
-			       <label for="">内容</label>
-             <textarea name="message" rows="4" cols="80" class="form-control" placeholder="内容" required maxlength="2000"></textarea>
-    		</div>
-    <div class="col-md-12">
-      <h4 class="text-center text-white"> <button type="submit" class="btn btn-warning text-white" style="background:#0099ff;">メッセージ送信</button></h4>
-
     </div>
-      </div>
-      {{-- <div class="modal-footer ">
-
-        {{-- <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button> --}}
-  {{-- </div> --}}
-      </form>
-    </div>
-  </div>
 </div>
