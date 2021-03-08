68455e68519a2521f400a0a6b14fd7fe3c8cc396
Sabik safkat (windows) <safkatsabik@gmail.com>
2021-03-08 09:49:09 +0600

    prefecture list and breadcrum link fixed

--- resources/views/front/layouts/project-details-breadcrump.blade.php
+++ resources/views/front/layouts/project-details-breadcrump.blade.php
@@ -7,7 +7,7 @@
 		<div class="col-12">
 			<ul class="list-inline project_category_data pt-4">
 				{{-- <li class="list-inline-item">>Top ></a></li> --}}
-				<li class="list-inline-item"> <a href="{{route('front-home')}}">TOP</a>> <a href="{{route('front-project-list')}}">プロジェクト一覧</a> > <a href="{{route('front-project-list', ['c' => $project->category->id])}}">{{ $project->category->name }}</a> >  {{ $project->title }}</a></li>
+				<li class="list-inline-item"> <a href="{{route('front-home')}}">TOP</a>> <a href="{{route('front-project-list-bycat', ['s' => 'd'])}}">プロジェクト一覧</a> > <a href="{{route('front-project-list-bycat', ['c' => $project->category->id, 's' => 'd'])}}">{{ $project->category->name }}</a> >  {{ $project->title }}</a></li>
 			</ul>
 		</div>
 	</div>
--- resources/views/user/layouts/prefectures.blade.php
+++ resources/views/user/layouts/prefectures.blade.php
@@ -1,15 +1,16 @@
 <?php
-	$Data = new App\Helpers\Data();
-	$prefectures = $Data->prefectures;
+$Data = new App\Helpers\Data();
+$prefectures = $Data->prefectures;
 ?>
 
 <select class="form-control required prefectures" id="prefectures" name="prefectures" onclick="removealert('prefecture_error')">
 	<option value="">選択</option>
 	<?php foreach($prefectures as $p){?>
-		<option value="{{$p['value']}}" {{ isset($user->profile->prefectures) && old('prefectures', $user->profile->prefectures) == $p['value'] ? 'selected' : '' }}>{{$p['name']}}</option>
+	<option value="<?php echo $p['value']; ?>" <?php if(isset($user->profile->prefectures) && old('prefectures', $user->profile->prefectures) == $p['value']){echo 'selected';} ?>>{{$p['name']}}</option>
 	<?php }?>
 </select>
 <span class="error_font" id="prefecture_error" style="color:red;"></span>
+
 
 @section('sort_js')
 
